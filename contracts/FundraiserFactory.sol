047f905d7bbf7db1215e4c166a43813f3f06d789
Bruce Yinhe <bruce.yinhe@gmail.com>
2021-06-11 00:25:35 +0200

    Add event fundraiserCreated

--- contracts/FundraiserFactory.sol
+++ contracts/FundraiserFactory.sol
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.9.0;
 
-contract FundraiserFactory {
+import "./Fundraiser.sol";
 
+contract FundraiserFactory {
+    Fundraiser[] private _fundraisers;
+
+    event fundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
+
+    function fundraisersCount() public view returns(uint256) {
+        return _fundraisers.length;
+    }
+
+    function createFundraiser( string memory name
+                             , string memory url
+                             , string memory imageURL
+                             , string memory description
+                             , address payable beneficiary
+                             ) public {
+        Fundraiser f = new Fundraiser( name
+                                     , url
+                                     , imageURL
+                                     , description
+                                     , beneficiary
+                                     , msg.sender
+                                     );
+        _fundraisers.push(f);
+        emit fundraiserCreated(f, msg.sender);
+    }
 }
--- test/fundraiser_factory_test.js
+++ test/fundraiser_factory_test.js
@@ -5,3 +5,36 @@ contract("FundraiserFactory: deployment", (accounts) => {
         assert(FundraiserFactoryContract.deployed(), "fundraiser factory was not deployed");
     });
 });
+
+contract("FundraiserFactory: createFundraiser", (accounts) => {
+    let fundraiserFactory;
+    const name          = "Beneficiary Name";
+    const url           = "beneficiaryname.org";
+    const imageURL      = "https://beneficiaryname.org/image.png";
+    const description   = "Beneficiary description";
+    const beneficiary   = accounts[1];
+
+    it("increments the fundraiserCount", async() => {
+        fundraiserFactory = await FundraiserFactoryContract.deployed();
+        const count0 = await fundraiserFactory.fundraisersCount();
+        await fundraiserFactory.createFundraiser( name
+                                                , url
+                                                , imageURL
+                                                , description
+                                                , beneficiary
+                                                );
+        const count1 = await fundraiserFactory.fundraisersCount();
+        assert.equal(count1-count0, 1, "fundraisersCount should increment by 1");
+    });
+
+    it("emits fundraiserCreated event", async() => {
+        fundraiserFactory = await FundraiserFactoryContract.deployed();
+        const tx = await fundraiserFactory.createFundraiser( name
+                                                           , url
+                                                           , imageURL
+                                                           , description
+                                                           , beneficiary
+                                                           );
+        assert.equal(tx.logs[0].event, "fundraiserCreated", "event name should match");
+    });
+});
